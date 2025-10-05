0e967914b21ba49a4b5b42a09bca1f47b9d468ab
abhilash-sourse <abhilash6480@gmail.com>
2025-10-05 22:43:44 +0530

    all

--- java/hello.java
+++ java/hello.java
@@ -1,5 +1,11 @@
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hello worls");
+class hello {
+    public static void main(String[] args){
+        // System.out.println("hello world");
+        // System.out.println("i am abhi");
+
+        double a = 10.3;
+        System.out.println(a);
+
+
     }
 }
