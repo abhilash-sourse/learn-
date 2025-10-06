d25f0f0daadef0eca7de792085fdaaa7c131bf6c
abhilash-sourse <abhilash6480@gmail.com>
2025-10-06 22:14:39 +0530

    all

--- java/hello.java
+++ java/hello.java
@@ -1,10 +1,134 @@
-class hello {
+ import java.util.Scanner;
+
+public class hello {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         // System.out.println("hello world");
         // System.out.println("i am abhi");
 
-        double a = 10.3;
-        System.out.println(a);
+        // int a = 10;
+        // int b = 20;
+        // System.out.println("the value of a is "+ a);
+        // System.out.println("the value of b is "+ b);
+
+        // double price = 200;
+        // double per_seat = 100;
+        // System.out.println("the total cost is "+ price);
+        // System.out.println("the cost of per seat "+ per_seat);
+
+        // String fname = "Abhi";
+        // String lname= "lash";
+        // System.out.println("The person first name  " + fname);
+        // System.out.println("The person last name " + lname);
+
+        // char grade = 'A';
+        // char currency = '$';
+        // System.out.println("Your class grade " + grade);
+        // System.out.println("Your currency is " + currency);
+
+        // boolean isStudent = true;
+        // boolean forsale = false;
+        // System.out.println("he is student "+ isStudent);
+        // System.out.println("the sale is books " + forsale);
+
+        // if(isStudent){
+        //     System.out.println("you are a student");
+        // }else{
+        //     System.out.println("you are not a student");
+        // }
+
+        // System.out.print("Enter your name:" );
+        // String name = scanner.nextLine();
+        // System.out.println("hello "+ name);
+
+        // System.out.print("Enter you age:");
+        // int age = scanner.nextInt();
+        // System.out.println("he age is " + age);
+
+        // System.out.print("Enter your scgp:");
+        // double scgp = scanner.nextDouble();
+        // System.out.print("your scgp: "+scgp);
+
+        // double width ;
+        // double height ;
+        // double area ;
+
+        // System.out.print("Enter the width:");
+        // width = scanner.nextDouble();
+
+        // System.out.print("Enter the height:");
+        // height = scanner.nextDouble();
+
+        // area = width * height;
+        // System.out.println("The area is : "+area + "cm²");
+
+        //MAD LIBS GAME
+
+        // String adjective1;
+        // String none1;
+        // String adjective2;
+        // String verb1;
+        // String adjective3;
+
+        // System.out.print("Enter an adjective (description):");
+        // adjective1 =scanner.nextLine();
+        // System.out.print("Enter a none (animal or person):");
+        // none1 = scanner.nextLine();
+        // System.out.print("Enter an adjective (decription):");
+        // adjective2 = scanner.nextLine();
+        // System.out.print("Enter a verb end with -ing  (action):");
+        // verb1 = scanner.nextLine();
+        // System.out.print("Enter an adjective (description):");
+        // adjective3 = scanner.nextLine();
+
+        // System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        // System.out.println("in an exhibit , i saw a " + none1 + ".");
+        // System.out.println(none1 +" was " + adjective2 + " and " + verb1 + "!");
+        // System.out.println("i was " + adjective3 + "!");
+        
+        
+        // int x = 10;
+        // int y = 3;
+        // int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+
+        // System.out.println(z);
+
+        // Augmented Assignment Operator
+
+        // x = x + y;
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // Increment and decrement operators
+
+        // x += 1;
+        // x++;
+        // x--;
+
+        // System.out.println(x);
+
+
+        //ORDER OF OPERATION [P-E-M-D-A-S] = Parentheses,Exponents,Mult/Div (left to right), Add/Sub (left to right)
+
+        // Double result = 3 + 4 * (7 - 5) / 2.0;
+
+        // System.out.println(result);
+
+    
+
+
+
+        scanner.close();
+        
 
 
     }
