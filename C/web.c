55747849d69f79ed02e35bab86f8c07fe5c07080
abhilash-sourse <abhilash6480@gmail.com>
2025-10-12 23:03:22 +0530

    all

---
+++ C/web.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(){
+
+    // char greetings[] = "Hello World!";
+    // greetings[0] = 'J';
+
+    // printf("%s", greetings);
+    
+    // printf("File :%s\n", __FILE__);
+    // printf("Date :%s\n", __DATE__);
+    // printf("Time :%s\n", __TIME__);
+    // printf("Line :%d\n", __LINE__);
+    // printf("ANSI :%d\n", __STDC__);
+
+    // printf("%f", sqrt(16)); // square root
+    // printf("%f", ceil(1.4)); // round up (round)
+    // printf("%f", floor(1.4)); // round up (round)
+    // printf("%f", pow(4, 3));
+
+    // fopen()	open a new or existing file
+    // fprintf()	write data to file
+    // fscanf()	read data from a file
+    // fputc()	write a character to file
+    // fgetc()	read a character from a file
+    // fclose()	close the file
+    // fseek()	set the file pointer to the given position
+    // fputw()	Write an integer to a file
+    // fgetw()	read an integer from a file
+    // ftell()	returns the current position
+    // rewind()	set the file pointer to the beginning of the file
+
+
+
+
+
+
+    
+    return 0;
+}
