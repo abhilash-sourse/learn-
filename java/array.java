8bedf289fdae87064077349f32bcfb74025f7f8b
abhilash-sourse <abhilash6480@gmail.com>
2025-10-14 23:59:36 +0530

    all

---
+++ java/array.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+                    //array
+    // String[] fruits = {"apple","abhi","lash","abhilash","banana","orange"};
+    // System.out.println(fruits[1]);
+
+
+    // for(String fruit : fruits){
+    //     System.out.println(fruit);
+    // }
+
+
+    // int num = fruits.length;
+    // System.out.println(num);
+
+
+    // Arrays.sort(fruits);
+    // Array.fill(fruits, "abhi");
+    
+
+                            //user array
+    
+    // String[] foods;
+    // int size;
+
+    // System.out.print("what # of food do you want?:");
+    // size = scanner.nextInt();
+    // scanner.nextLine();
+
+    // foods = new String[size];
+
+    // for(int i = 0;i<foods.length;i++){
+    //     System.out.print("Enter a food:");
+    //     foods[i] = scanner.nextLine();
+    // }
+
+    // for(String food :foods){
+    //     System.out.println(food);
+    // }
+
+
+                        // search an array
+
+
+    // String[] fruits = {"apple","abhi","lash","abhilash","banana","orange"};
+    // String target = "apple";
+    // boolean isFound = false;
+
+    // for(int i = 0;i<fruits.length;i++){
+    //     if(fruits[i].equals(target)){
+    //         System.out.println("Element found at index:" + i);
+    //         isFound = true;
+    //         break;
+
+    //     }
+    // }
+
+    // if(!isFound){
+    //     System.out.println("Element not found in the array");
+    // }
+
+
+
+
+    int[] numbers = {1,2,3,4,5,6,7};
+    int target = 6;
+    boolean isFound = false;
+
+    for(int i = 0;i<numbers.length;i++){
+        if(target == numbers[i]){
+            System.out.println("Element found at index:" + i);
+            isFound = true;
+            break;
+
+        }
+    }
+
+    if(!isFound){
+        System.out.println("Element not found in the array");
+    }
+
+
+
+
+    scanner.close();
+
+
+    }
+}
