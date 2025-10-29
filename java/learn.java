ca253520326eab87601e23c09b1c2536439fef2a
abhilash-sourse <abhilash6480@gmail.com>
2025-10-29 20:57:10 +0530

    aLL

--- java/learn.java
+++ java/learn.java
@@ -1,17 +1,5 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class learn {
     public static void main(String args[]){
-        
-       Deque <String> a = new ArrayDeque<>();
-       a.addFirst("asd");
-       a.add("wedd");
-       a.add("wedddd");
-       a.add("weddaa");
-       System.out.println(a.pop());
-       System.out.println(a);
-        
         
     }
 }
---
+++ java/learn1.java
@@ -0,0 +1,5 @@
+public class learn1 {
+    public static void main(String[] args) {
+     
+    }
+}
